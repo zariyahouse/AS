518f51fdc4e9b1bb843830cba28461a5dc9fb8c5
zariyahouse <zariya.house@gmail.com>
2026-08-16 23:54:50 +0000

    Use local photo paths

--- frontend/src/config/weddingConfig.js
+++ frontend/src/config/weddingConfig.js
@@ -18,14 +18,14 @@ export const couple = {
     short: "Abel",
     parents: "Son of Rev. Fr. Koshy Thomas & Mrs. Beena Koshy",
     photo:
-      "https://drive.google.com/uc?export=view&id=1r8eHSpAOGOp2INiBbLmXnG0s7S-509cY",
+      "/images/abel.png",
   },
   bride: {
     name: "Merlyn Grace George",
     short: "Merlyn",
     parents: "Daughter of Mr. Shaji George & Mrs. Sheeja George",
     photo:
-      "https://drive.google.com/uc?export=view&id=16zdenBfrWL7q_8MtxHnYR1_DjBxTF3k2",
+      "/images/merlyn.png",
   },
 };
 
@@ -128,8 +128,8 @@ export const dressCodeSwatches = {
 
 // ---- Gallery (swap URLs freely; add more entries anytime) ----
 export const galleryImages = [
-  "https://drive.google.com/uc?export=view&id=16zdenBfrWL7q_8MtxHnYR1_DjBxTF3k2",
-  "https://drive.google.com/uc?export=view&id=1r8eHSpAOGOp2INiBbLmXnG0s7S-509cY",
+    "/images/merlyn.png",
+  "/images/abel.png",
 ];
 
 // ---- Jigsaw source (fixed, landscape ~4:3, centred subject) ----
